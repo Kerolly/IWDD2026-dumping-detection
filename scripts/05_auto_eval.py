f9d9345127b2ca3aa358b15dbb2ab1ff8eb54041
Andrei Ticarat <ticarat.andrei125@gmail.com>
2025-12-04 22:35:58 +0200

    Test V8

--- scripts/05_auto_eval.py
+++ scripts/05_auto_eval.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 # --- CONFIGURATION ---
 # Directory containing prediction text files (.txt)
-PREDICTIONS_DIR = 'resultsV7'
+PREDICTIONS_DIR = 'resultsV8_H'
 
 # Directory containing ground-truth annotation files (.json)
 GROUND_TRUTH_DIR = 'data/annotations'
